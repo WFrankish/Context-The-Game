137cb826e4b167a6cfca6acb0b4b8d17dd7e6b7d
William Frankish <wfrankish@gmail.com>
2020-04-10 10:02:03 +0100

    add basic setup

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-const x = 1;
+let canvas: HTMLCanvasElement;
+let ctx: CanvasRenderingContext2D;
+
+const main = () => {
+    canvas = document.getElementsByTagName("canvas")[0];
+    ctx = canvas.getContext("2d")!;
+}
+
+main();
