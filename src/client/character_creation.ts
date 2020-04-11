ef4c90064f3ab7e1fa758d3317a9dc42748ee896
agata-borkowska <borkowska.agata37@gmail.com>
2020-04-11 15:40:02 +0200

    using display.ts to display a wizard and arrows

--- src/client/character_creation.ts
+++ src/client/character_creation.ts
@@ -6,22 +6,34 @@ let position: Vector2|null = null;
 export const creationInit = () => {
     display.clear();
     display.drawHud(context => {
+        let curr = 0;
+        let presets: Array<string>;
+        presets = ['./assets/gandalf_the_girthy_green.png', './assets/gandalf_the_girthy_blue.png', './assets/gandalf_the_girthy_red.png', './assets/gandalf_the_girthy_purple.png']; 
         let img = new Image();
         img.src = './assets/gandalf_the_girthy.png';
         context.imageSmoothingEnabled = false;
         img.onload = function() {context.drawImage(img, 0, 0, 256, 256)};
-        // context.fillStyle = '#eee';
-        // context.fillRect(10, 10, display.width - 20, display.height - 20);
-        // context.fillStyle = '#f00';
-        // context.fillRect(
-        //     0.5 * display.width - 10, 0.5 * display.height - 10, 20, 20);
-        // context.font = '20pt sans-serif';
-        // context.textAlign = 'center';
-        // context.textBaseline = 'bottom';
-        // if (position) {
-        // context.fillText(Math.round(position.x) + ',' + Math.round(position.y),
-        //                     0.5 * display.width, 0.5 * display.height - 20);
-        // }
+        let arrowLeft = new Image();
+        arrowLeft.src = './assets/arrow_left.png';
+        arrowLeft.onclick = function() {
+            img.src = presets[(curr - 1) % presets.length];
+            img.onload = function() {
+                context.drawImage(img, 0, 0, 256, 256)
+            };
+        }
+        arrowLeft.onload = function() {
+            context.drawImage(arrowLeft, 0, 256, 64, 32);
+        };
+        let arrowRight = new Image();
+        arrowRight.src = './assets/arrow_right.png';
+        arrowRight.onclick = function() {
+            img.src = presets[(curr + 1) % presets.length];
+            context.drawImage(img, 0, 0, 256, 256);
+
+        }
+        arrowRight.onload = function() {
+            context.drawImage(arrowRight, 160, 256, 64, 32);
+        }
     });
       
     display.onMouseMove(event => {
