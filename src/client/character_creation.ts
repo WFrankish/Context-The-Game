7953d8c05924e9fbf473ca8272ea756ad9409a74
agata-borkowska <borkowska.agata37@gmail.com>
2020-04-11 18:59:13 +0200

    what even is this

--- src/client/character_creation.ts
+++ src/client/character_creation.ts
@@ -1,6 +1,7 @@
 import * as display from './display.js';
 import { Vector2 } from '../common/vector2.js';
 import { HudPiece, Anchor, Tile, Drawable } from './drawing/drawable.js';
+import { StaticImage, LoopingImage, Image } from "./drawing/image.js";
 
 let position: Vector2 | null = null;
 let curr = 0;
@@ -13,8 +14,6 @@ presets = [
 ];
 window.requestAnimationFrame(creationInit);
 function creationInit (totalMilliseconds: number): void  {
-
-
   display.draw(
     (context) => {
       let img = new Image();
@@ -23,18 +22,21 @@ function creationInit (totalMilliseconds: number): void  {
       img.onload = function () {
         context.drawImage(img, 0, 0, 256, 256);
       };
-    },
+
+      let arrowLeft = new StaticImage('arrow_left.png');
+      if (arrowLeft.isLoaded) {
+        //context.drawImage(arrowLeft, 0, 256, 64, 32);
+        let arrLeft = new HudPiece(arrowLeft, new Vector2(0, 0), Anchor.TopLeft);
+        arrLeft.draw(context, 0);
+      };
+      let arrowRight = new StaticImage("arrow_right.png")
+      if (arrowRight.isLoaded) {
+        //context.drawImage(arrowRight, 160, 256, 64, 32);
+        let arrRight = new HudPiece(arrowRight, new Vector2(128, 0), Anchor.TopLeft);
+        arrRight.draw(context, 0);
+      };
+          },
     (context) => {
-      let arrowLeft = new Image();
-      arrowLeft.src = '../assets/arrow_left.png';
-      arrowLeft.onload = function () {
-        context.drawImage(arrowLeft, 0, 256, 64, 32);
-      };
-      let arrowRight = new Image();
-      arrowRight.src = '../assets/arrow_right.png';
-      arrowRight.onload = function () {
-        context.drawImage(arrowRight, 160, 256, 64, 32);
-      };
     }
   );
     display.onMouseMove((event) => {
