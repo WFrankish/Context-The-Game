732fccf3aa4a8305ae33082be9882868e6bb8554
agata-borkowska <borkowska.agata37@gmail.com>
2020-04-11 16:57:41 +0200

    moving character creation to a separate page

--- src/client/character_creation.ts
+++ src/client/character_creation.ts
@@ -1,24 +1,24 @@
-import * as display from "./display.js";
-import { Vector2 } from "../common/vector2.js";
-import { HudPiece, Anchor, Tile, Drawable } from "./drawing/drawable.js";
+import * as display from './display.js';
+import { Vector2 } from '../common/vector2.js';
+import { HudPiece, Anchor, Tile, Drawable } from './drawing/drawable.js';
 
 let position: Vector2 | null = null;
 let curr = 0;
 let presets: Array<string>;
 presets = [
-  "./assets/gandalf_the_girthy_green.png",
-  "./assets/gandalf_the_girthy_blue.png",
-  "./assets/gandalf_the_girthy_red.png",
-  "./assets/gandalf_the_girthy_purple.png",
+  '../assets/gandalf_the_girthy_green.png',
+  '../assets/gandalf_the_girthy_blue.png',
+  '../assets/gandalf_the_girthy_red.png',
+  '../assets/gandalf_the_girthy_purple.png',
 ];
+window.requestAnimationFrame(creationInit);
+function creationInit (totalMilliseconds: number): void  {
 
-export const creationInit = () => {
-  display.clear();
 
   display.draw(
     (context) => {
       let img = new Image();
-      img.src = "./assets/gandalf_the_girthy.png";
+      img.src = presets[curr];
       context.imageSmoothingEnabled = false;
       img.onload = function () {
         context.drawImage(img, 0, 0, 256, 256);
@@ -26,19 +26,18 @@ export const creationInit = () => {
     },
     (context) => {
       let arrowLeft = new Image();
-      arrowLeft.src = "./assets/arrow_left.png";
+      arrowLeft.src = '../assets/arrow_left.png';
       arrowLeft.onload = function () {
         context.drawImage(arrowLeft, 0, 256, 64, 32);
       };
       let arrowRight = new Image();
-      arrowRight.src = "./assets/arrow_right.png";
+      arrowRight.src = '../assets/arrow_right.png';
       arrowRight.onload = function () {
         context.drawImage(arrowRight, 160, 256, 64, 32);
       };
     }
   );
-
-  display.onMouseMove((event) => {
+    display.onMouseMove((event) => {
     position = event.position;
   });
 };
--- src/client/main.ts
+++ src/client/main.ts
@@ -1,12 +1,13 @@
 import * as engine from './engine.js';
-import {creationInit} from './character_creation.js';
 
-const kill = () => {
-  engine.kill();
-};
+const kill =
+    () => {
+      engine.kill();
+    }
 
-const main = () => {
-  engine.run();
-};
+const main =
+    () => {
+    engine.run();
+}
 
 main();
