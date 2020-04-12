be65b8cd17ee4e21f61d79956135ceb9ac650ad5
Joe Fowler <scrumplesplunge@gmail.com>
2020-04-12 01:06:37 +0100

    Reorder display.ts to put exported bits first.

--- src/client/display.ts
+++ src/client/display.ts
@@ -1,11 +1,51 @@
 import { Transform } from '../common/transform.js';
 import { Vector2 } from '../common/vector2.js';
 
-const canvas = document.querySelector('canvas')!;
-const context = canvas.getContext('2d')!;
-
 export const width = 640;
 export const height = 480;
+
+// Apply the suitable transform for drawing the user interface over the game
+// world. The callback will receive the context it needs to draw, and can assume
+// a resolution of (display.width, display.height) which will be scaled to fit.
+export function draw(callback: (context: CanvasRenderingContext2D) => void): void {
+  context.setTransform(1, 0, 0, 1, 0, 0);
+  context.fillStyle = 'black';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  const { scale, offset } = computeScale();
+  context.translate(offset.x, offset.y);
+  context.scale(scale, scale);
+  context.imageSmoothingEnabled = false;
+
+  context.fillStyle = 'white';
+  context.fillRect(0, 0, width, height);
+
+  callback(context);
+}
+
+// Mouse events for the canvas, translated into the same coordinate space as the transform used by draw().
+export type MouseAction = 'up' | 'down' | 'move';
+export type MouseButton = 'left' | 'right';
+export interface MouseEvent {
+  // Position of the mouse cursor in virtual coordinates.
+  position: Vector2;
+}
+export interface MouseButtonEvent extends MouseEvent {
+  button: MouseButton;
+}
+export type Handler<T> = (event: T) => void;
+export function onMouseMove(handler: Handler<MouseEvent>): void {
+  mouseMoveHandlers.add(handler);
+}
+export function onMouseUp(handler: Handler<MouseButtonEvent>): void {
+  mouseUpHandlers.add(handler);
+}
+export function onMouseDown(handler: Handler<MouseButtonEvent>): void {
+  mouseDownHandlers.add(handler);
+}
+
+const canvas = document.querySelector('canvas')!;
+const context = canvas.getContext('2d')!;
 let screenToCanvas = Transform.identity();
 
 function computeScale(): { scale: number; offset: Vector2 } {
@@ -29,48 +69,9 @@ function roundScale(val: number): number {
   return 1 <= val && val <= 2.5 ? Math.floor(val) : val;
 }
 
-// Apply the suitable transform for drawing the user interface over the game
-// world. The callback will receive the context it needs to draw, and can assume
-// a resolution of (display.width, display.height) which will be scaled to fit.
-export function draw(callback: (context: CanvasRenderingContext2D) => void): void {
-  context.setTransform(1, 0, 0, 1, 0, 0);
-  context.fillStyle = 'black';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-
-  const { scale, offset } = computeScale();
-  context.translate(offset.x, offset.y);
-  context.scale(scale, scale);
-  context.imageSmoothingEnabled = false;
-
-  context.fillStyle = 'white';
-  context.fillRect(0, 0, width, height);
-
-  callback(context);
-}
-
-export type MouseAction = 'up' | 'down' | 'move';
-export type MouseButton = 'left' | 'right';
-export interface MouseEvent {
-  // Position of the mouse cursor in virtual coordinates.
-  position: Vector2;
-}
-export interface MouseButtonEvent extends MouseEvent {
-  button: MouseButton;
-}
-export type Handler<T> = (event: T) => void;
-
 const mouseMoveHandlers: Set<Handler<MouseEvent>> = new Set();
 const mouseUpHandlers: Set<Handler<MouseButtonEvent>> = new Set();
 const mouseDownHandlers: Set<Handler<MouseButtonEvent>> = new Set();
-export function onMouseMove(handler: Handler<MouseEvent>): void {
-  mouseMoveHandlers.add(handler);
-}
-export function onMouseUp(handler: Handler<MouseButtonEvent>): void {
-  mouseUpHandlers.add(handler);
-}
-export function onMouseDown(handler: Handler<MouseButtonEvent>): void {
-  mouseDownHandlers.add(handler);
-}
 canvas.addEventListener('mousemove', (event) => {
   const mouseEvent: MouseEvent = {
     position: screenToCanvas.apply(new Vector2(event.x, event.y)),
