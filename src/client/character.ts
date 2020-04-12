eb51cdfd09163acfaaae856e2d1b71dcce050600
William Frankish <wfrankish@gmail.com>
2020-04-12 11:16:01 +0100

    split out inputs from character, for camera

--- src/client/character.ts
+++ src/client/character.ts
@@ -1,50 +1,7 @@
 import { Seconds } from '../common/time.js';
 import { Vector2 } from '../common/vector2.js';
 import { clamp } from '../common/utils.js';
-
-import * as display from './display.js';
-
-type Input = 'up' | 'down' | 'left' | 'right' | 'primary' | 'secondary';
-class Inputs {
-  up = 0;
-  down = 0;
-  left = 0;
-  right = 0;
-  primary = 0;
-  secondary = 0;
-}
-const inputs = new Inputs();
-const keyBindings: Map<string, Input> = new Map([
-  ['KeyW', 'up'],
-  ['KeyA', 'left'],
-  ['KeyS', 'down'],
-  ['KeyD', 'right'],
-]);
-const mouseBindings: Map<number, Input> = new Map([
-  [0, 'primary'],
-  [2, 'secondary'],
-]);
-addEventListener('keydown', (event: KeyboardEvent) => {
-  if (!keyBindings.has(event.code)) return;
-  const action = keyBindings.get(event.code)!;
-  inputs[action] = 1;
-});
-addEventListener('keyup', (event: KeyboardEvent) => {
-  if (!keyBindings.has(event.code)) return;
-  const action = keyBindings.get(event.code)!;
-  inputs[action] = 0;
-});
-addEventListener('contextmenu', (event: MouseEvent) => event.preventDefault());
-addEventListener('mousedown', (event: MouseEvent) => {
-  if (!mouseBindings.has(event.button)) return;
-  const action = mouseBindings.get(event.button)!;
-  inputs[action] = 1;
-});
-addEventListener('mouseup', (event: MouseEvent) => {
-  if (!mouseBindings.has(event.button)) return;
-  const action = mouseBindings.get(event.button)!;
-  inputs[action] = 0;
-});
+import { inputs, Inputs } from "./inputs.js";
 
 enum Direction {
   RIGHT = 0,
