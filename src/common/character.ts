9235c49e397f0291c59c90af83616eebb17e1773
Joe Fowler <scrumplesplunge@gmail.com>
2020-04-13 20:27:31 +0100

    Network the player coordinates.

--- src/common/character.ts
+++ src/common/character.ts
@@ -11,7 +11,32 @@ export class Character {
   }
   update(dt: Seconds) {
     if (this.isLocal) this.processInputs();
-    this.position = this.position.add(this.velocity.multiply(dt));
+    this._position = this._position.add(this._velocity.multiply(dt));
+  }
+  get position() {
+    return this._position;
+  }
+  get velocity() {
+    return this._velocity;
+  }
+  set position(value: Vector2) {
+    if (this._position.equals(value)) return;
+    this._position = value;
+    this.sendMovements();
+  }
+  set velocity(value: Vector2) {
+    if (this._velocity.equals(value)) return;
+    this._velocity = value;
+    this.sendMovements();
+  }
+  sendMovements() {
+    if (!this.isLocal) return;
+    if (!this.channel) return;
+    this.channel.update({
+      type: 'Movement',
+      position: [this._position.x, this._position.y],
+      velocity: [this._velocity.x, this._velocity.y],
+    });
   }
   processInputs() {
     let move = new Vector2(this.inputs.right - this.inputs.left, this.inputs.down - this.inputs.up);
@@ -25,8 +50,8 @@ export class Character {
   isLocal = false;
   inputs = new Inputs();
   zone = 'default';
-  position = new Vector2(0, 0);
-  velocity = new Vector2(0, 0);
+  _position = new Vector2(0, 0);
+  _velocity = new Vector2(0, 0);
 }
 
 export interface Snapshot {
@@ -36,16 +61,42 @@ export interface Snapshot {
   // TODO: Add other synchronized properties such as player inventory.
 }
 
-export type Update = null;
+interface TeleportUpdate {
+  type: 'Teleport';
+  zone: string;
+}
+
+interface MovementUpdate {
+  type: 'Movement';
+  position: [number, number];
+  velocity: [number, number];
+}
+
+export type Update = TeleportUpdate | MovementUpdate;
 
 export class CharacterHandler {
   constructor(character: Character) {
     this.character = character;
   }
-  applyUpdate(state: Snapshot, update: Update): void {}
+  applyUpdate(state: Snapshot, update: Update): void {
+    switch (update.type) {
+      case 'Teleport':
+        state.zone = update.zone;
+        break;
+      case 'Movement':
+        state.position = update.position;
+        state.velocity = update.velocity;
+        break;
+    }
+  }
   onChange(state: Snapshot): void {
-    this.character.position = new Vector2(state.position[0], state.position[1]);
-    this.character.velocity = new Vector2(state.velocity[0], state.velocity[1]);
+    if (!this.character.isLocal || !this.initialized) {
+      this.initialized = true;
+      this.character.zone = state.zone;
+      this.character.position = new Vector2(state.position[0], state.position[1]);
+      this.character.velocity = new Vector2(state.velocity[0], state.velocity[1]);
+    }
   }
   character: Character;
+  initialized = false;
 }
